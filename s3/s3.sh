d4c2351afd452ebdebd62c67a5122e0f61ff9dbc
sawsansalah <sawsan.salaheldin@gmail.com>
2023-07-26 17:50:18 +0300

    Create s3.sh

---
+++ s3/s3.sh
@@ -0,0 +1,32 @@
+#create new bucket
+aws s3api create-bucket --bucket devops90-cli-bucket --region eu-north-1 --create-bucket-configuration LocationConstraint=eu-north-1
+
+#get list of all available buckets in the default region of the current user
+aws s3 ls
+
+#block all public access
+aws s3api put-public-access-block --bucket devops90-cli-bucket --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"
+
+#upload image file to the bucket
+aws s3api put-object --bucket devops90-cli-bucket --content-type image/jpeg --key codeDeploy_in_action.jpg --body /mnt/c/aws/codeDeploy_in_action.jpg
+
+#disable block public access (unblock)
+aws s3api delete-public-access-block --bucket devops90-cli-bucket
+
+#make the image file accessible for the world
+aws s3api put-object-acl --bucket devops90-cli-bucket --key codeDeploy_in_action.jpg --grant-read uri=http://acs.amazonaws.com/groups/global/AllUsers
+
+#make the image file accessible for the world
+aws s3api put-object-acl --bucket devops90-cli-bucket --key codeDeploy_in_action.jpg --acl public-read
+
+#make the image file private
+aws s3api put-object-acl --bucket devops90-cli-bucket --key codeDeploy_in_action.jpg --acl private
+
+#delete the image file
+aws s3api delete-object --bucket devops90-cli-bucket --key codeDeploy_in_action.jpg
+
+#delete the bucket
+aws s3api delete-bucket --bucket devops90-cli-bucket --region eu-north-1
+
+#get list of all available buckets in the default region of the current user
+aws s3 ls
