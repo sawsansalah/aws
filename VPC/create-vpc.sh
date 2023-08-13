d76a98f5a6fe3c06a76f0ac55dfff9f5ff6b0b67
Salah <sawsan.salah@dell.com>
2023-08-14 00:01:24 +0300

    add pvc

--- VPC/create-vpc.sh
+++ VPC/create-vpc.sh
@@ -3,8 +3,8 @@ vpc_result=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --region eu-north-1  --
 
 echo $vpc_result
 
-vpc_id =$(echo $vpc_result)
-echo $vpc_id
+vpc_Id=$(echo $vpc_result | grep -oP '(?<="VpcId": ")[^"]*')
+
 
 #aws ec2 create-subnet --vpc-id $vpc_id --cidr-block 10.0.1.0/24 --tag-specifications ResourceType=subnet,Tags=[{Key=Name,Value=first-public-subnet}] --availability-zone eu-north-1a --availability-zone-id eun1-az1
 
