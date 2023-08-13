97c585e424cab4e7547173a9179d084a6565eb5d
Salah <sawsan.salah@dell.com>
2023-08-13 23:40:39 +0300

    add pvc

--- VPC/create-vpc.sh
+++ VPC/create-vpc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-vpc_result=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --region eu-north-1  --tag-specification ResourceType=vpc,Tags="[{Key=Name,Value=Devops90-vpc}]" --output python)
+vpc_result=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --region eu-north-1  --tag-specification ResourceType=vpc,Tags="[{Key=Name,Value=Devops90-vpc}]" --output json)
 
 echo $vpc_result
 
