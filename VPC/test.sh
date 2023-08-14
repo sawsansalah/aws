d6e3b5ae9495f97ca82a4e4e3c49594ba4b31b4a
Salah <sawsan.salah@dell.com>
2023-08-14 21:33:38 +0300

    add new script

---
+++ VPC/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# create vpc 10.0.0.0/16
+vpc_result=$(aws ec2 create-vpc \
+    --cidr-block 10.0.0.0/16 \
+    --tag-specification ResourceType=vpc,Tags="[{Key=Name,Value=Devops90-vpc}]" \
+    --region eu-north-1 \
+    --output json)
+echo $vpc_result
+
+vpc_id=$(echo $vpc_result |  grep -oP '(?<="VpcId": ")[^"]*')
+echo $vpc_id
+
+# Allow Error handling per vpc_id
+if ["$vpc_id" == ""];then
+   echo "Error in creating vpc"
+   exit 1 
+fi    
