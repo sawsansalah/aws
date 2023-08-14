5b716603231401a43c81c7081fb285ee4ddd61cc
Salah <sawsan.salah@dell.com>
2023-08-15 00:23:15 +0300

    add new script

--- VPC/test.sh
+++ VPC/test.sh
@@ -19,7 +19,31 @@ if [ "$vpc_check" == "" ]; then
     exit 1 
     fi    
 
+
     echo "vpc created "
 else
     echo "VPC already exsit"
 fi  
+#---------------------------------------------
+# create first-public-subnet in first zone Az1
+subnet_check=$(aws ec2 describe-subnets --region eu-north-1 --filters --filters  Name=tag:Name,Values=Devops-public-zone-1 | grep -oP '(?<="SubnetId": ")[^"]*')
+if [ "$subnet_check" == "" ];then
+    echo "subnet 1 will be created... "
+    subnet_result=$(aws ec2 create-subnet \
+        --vpc-id $vpc_id \
+        --cidr-block 10.0.1.0/24 \
+        --tag-specificatio1s ResourceType=subnet,Tags="[{Key=Name,Value=Devops-public-zone-1}]" \
+        --availability-zone eu-north-1a	 \
+        --availability-zone-id eun1-az1   \
+        --output json)
+    subnet_id=$(echo $subnet_result |  grep -oP '(?<="SubnetId": ")[^"]*')   
+    echo $subnet_id 
+     # Allow Error handling per vpc_id
+    if [ "$subnet_id" == "" ]; then
+    echo "Error in creating subnet1"
+    exit 1 
+    fi   
+else
+    subnet_id=$subnet_check
+    echo $subnet_id
+fi
