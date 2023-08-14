9fc1c9237f42b048b99a960b85843aae79fb9c7c
Salah <sawsan.salah@dell.com>
2023-08-14 21:22:27 +0300

    add new script

--- VPC/test.sh
+++ VPC/test.sh
@@ -3,7 +3,7 @@
 vpc_result=$(aws ec2 create-vpc \
     --cidr-block 10.0.0.0/16 \
     --tag-specification ResourceType=vpc,Tags="[{Key=Name,Value=Devops90-vpc}]" \
-    --region eu-north-1
+    --region eu-north-1 \
     --output json)
 echo $vpc_result
 
