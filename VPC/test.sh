b6c6484cf76c14f0fed8af8789bc60e75ed9b078
Salah <sawsan.salah@dell.com>
2023-08-14 22:12:15 +0300

    add new script

--- VPC/test.sh
+++ VPC/test.sh
@@ -2,20 +2,24 @@
 #check vpc if found or not 
 vpc_check=$(aws ec2 describe-vpcs --filters tag Name=Name,Value=Devops90-vpc | grep -oP '(?<="VpcId": ")[^"]*')
 # create vpc 10.0.0.0/16
-vpc_result=$(aws ec2 create-vpc \
-    --cidr-block 10.0.0.0/16 \
-    --tag-specification ResourceType=vpc,Tags="[{Key=Name,Value=Devops90-vpc}]" \
-    --region eu-north-1 \
-    --output json)
-echo $vpc_result
+if [$"vpc_check" == ""],then
+    vpc_result=$(aws ec2 create-vpc \
+        --cidr-block 10.0.0.0/16 \
+        --tag-specification ResourceType=vpc,Tags="[{Key=Name,Value=Devops90-vpc}]" \
+        --region eu-north-1 \
+        --output json)
+    echo $vpc_result
 
-vpc_id=$(echo $vpc_result |  grep -oP '(?<="VpcId": ")[^"]*')
-echo $vpc_id
+    vpc_id=$(echo $vpc_result |  grep -oP '(?<="VpcId": ")[^"]*')
+    echo $vpc_id
 
-# Allow Error handling per vpc_id
-if ["$vpc_id" == ""];then
-   echo "Error in creating vpc"
-   exit 1 
-fi    
+    # Allow Error handling per vpc_id
+    if ["$vpc_id" == ""];then
+    echo "Error in creating vpc"
+    exit 1 
+    fi    
 
-echo "vpc created "
+    echo "vpc created "
+else
+  echo "VPC already exsit"
+fi  
