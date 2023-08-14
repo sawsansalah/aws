7de715df2ed604bb817acd891c35efcb3dac47d6
Salah <sawsan.salah@dell.com>
2023-08-14 22:29:05 +0300

    add new script

--- VPC/test.sh
+++ VPC/test.sh
@@ -2,7 +2,7 @@
 #check vpc if found or not 
 vpc_check=$(aws ec2 describe-vpcs --region eu-north-1 --filters  Name=tag:Name,Values=Devops90-vpc | grep -oP '(?<="VpcId": ")[^"]*')
 # create vpc 10.0.0.0/16
-if [$"vpc_check" == ""]; then
+if [$vpc_check == ""]; then
     vpc_result=$(aws ec2 create-vpc \
         --cidr-block 10.0.0.0/16 \
         --tag-specification ResourceType=vpc,Tags="[{Key=Name,Value=Devops90-vpc}]" \
@@ -20,7 +20,6 @@ if [$"vpc_check" == ""]; then
     fi    
 
     echo "vpc created "
-
 else
     echo "VPC already exsit"
 fi  
