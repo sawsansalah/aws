0aa93d10307d562159a1ec32463c97fd1d27bb15
Salah <sawsan.salah@dell.com>
2023-10-17 21:39:36 +0300

    add vpc.sh to tst

--- VPC/vpc.sh
+++ VPC/vpc.sh
@@ -65,7 +65,7 @@ if [ "$igw_check" == "" ]; then
     fi
     echo "IGW create
 else
-echo "igw already exist"
-igw_id=$igw_check
+    echo "igw already exist"
+    igw_id=$igw_check
+fi    
 echo $igw-id
-fi 
