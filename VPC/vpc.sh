cb1316614acf438747efbbc2657c6c141c78ba48
Salah <sawsan.salah@dell.com>
2023-10-17 13:14:12 +0300

    add vpc.sh to tst

--- VPC/vpc.sh
+++ VPC/vpc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # create vpc 10.0.0.0/16
-vpc_result=$(aws ec2 create-vpc  --cidr-block 10.0.0.0/15 --tag-specification ResourceType=vpc,Tags="[{Key=Name,Value=Devops90-VPC}]" --region us-west-2 --output json)
+vpc_result=$(aws ec2 create-vpc  --cidr-block 10.0.0.0/16 --tag-specification ResourceType=vpc,Tags="[{Key=Name,Value=Devops90-VPC}]" --region us-west-2 --output json)
 echo $vpc_result
 vpc_id=$(echo $vpc_result | grep -oP '(?<="VpcId": ")[^"]*')
 echo $vpc_id
