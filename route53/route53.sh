6d48d8c656f21c464efc57ac773d2068a42ee774
Salah <sawsan.salah@dell.com>
2023-10-25 15:32:02 +0300

    adjust script route53

---
+++ route53/route53.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+region="us-east-1"
+dns_name="devops90.link"
+
+create_hosted_zone()
+{
+  check_zone=$(aws route53 list-hosted-zones-by-name --dns-name $dns_name |  grep -oP '(?<="Id": ")[^"]*' | uniq)
+  if [ "$check_zone" == "" ]; then
+     echo " Hosted Zone will be created ....."
+     date=$(date -u +"%Y-%m-%d-%H-%M-%S")
+     hosted_zone_id=$(aws route53 create-hosted-zone --name $dns_name --caller-reference $date | grep -oP '(?<="Id": ")[^"]*' | uniq ")
+     if [ "$hosted_zone_id" == "" ];then
+        echo "Error in created hosted Zone..."
+        exit 1
+     fi
+     echo "hosted zone created $hosted_zone_id "    
+  else
+  echo "hosted zone already exsist..."
+  hosted_zone_id=$check_zone
+  echo "$hosted_zone_id"
+  fi
+}
+create_hosted_zone
