679ab51959ffa5dc769dcb66ebd3e22a31b2593e
Salah <sawsan.salah@dell.com>
2023-10-30 21:51:11 +0200

    add listner  to script

--- autoscaling/autoscaling.sh
+++ autoscaling/autoscaling.sh
@@ -74,7 +74,7 @@ create_TG(){
     tg_check=$(aws elbv2 describe-target-groups --region us-east-1  --query "TargetGroups[?TargetGroupName == 'devops90-Tg']"| grep -oP '(?<="TargetGroupArn": ")[^"]*') 
     if [ "$tg_check" == "" ]; then
        echo "TG will be created"
-       TG_ARN=$(aws elbv2 create-target-group --region us-east-1 --name devops90-Tg --protocol HTTP --port 8002 --vpc-id $vpc_id | grep -oP '(?<="TargetGroupArn": ")[^"]*') 
+       TG_ARN=$(aws elbv2 create-target-group --region us-east-1 --name devops90-Tg --protocol TCP --port 8002 --vpc-id $vpc_id | grep -oP '(?<="TargetGroupArn": ")[^"]*') 
        if ["$TG_ARN" == " " ]; then
           echo "Error in Creating TG" 
           exit 1
